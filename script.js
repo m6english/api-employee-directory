041ac353912602d57fbaf42c732ce76f78162c97
Matthew English <m6english@gmail.com>
2023-01-16 11:15:35 -0500

    removed img placeholder from html

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let employees = [];
-const urlAPI = `https://randomuser.me/api?results=12&inc=name, picture, email, location, phone, dob &noinfo &nat=US`
+const urlAPI = `https://randomuser.me/api?results=12&inc=name, picture, email, location, phone, dob &noinfo &nat=US`;
 const gridContainer = document.querySelector('.grid-container');
 const overlay = document.querySelector('.overlay');
 const modalContainer = document.querySelector('.modal-content');
@@ -9,7 +9,7 @@ fetch(urlAPI)
     .then(res => res.json())
     .then(res => res.results)
     .then(displayEmployees)
-    .catch(err => console.log(err));
+    .catch(err => console.log(err))
 
 function displayEmployees(employeeData) {
     employees = employeeData;
@@ -32,8 +32,8 @@ function displayEmployees(employeeData) {
     </div>
 </div>
     `
+    
+gridContainer.innerHTML = employeeHTML;
     }
-
 )};
 
-gridContainer.innerHTML = employeeHTML;
